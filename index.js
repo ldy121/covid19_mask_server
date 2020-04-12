5744d50f2fb945eca56e7db92efa866876f7762b
Lee <dy121.lee@gmail.com>
2020-04-12 20:09:40 +0900

    run prettier fix

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const axios	= require('axios');
-const console = require('console');
+const express = require("express");
+const axios = require("axios");
+const console = require("console");
 
-const storePage = 'https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/stores/json?page='
-const salePage  = 'https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/sales/json?page='
+const storePage =
+  "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/stores/json?page=";
+const salePage =
+  "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/sales/json?page=";
 
 const app = express();
 const port = 80;
@@ -11,68 +13,70 @@ const port = 80;
 let storeInfos = [];
 
 async function req(uriBase, key) {
-	let page = 1;
-	let totalPages = 1;
-	let ret = [];
+  let page = 1;
+  let totalPages = 1;
+  let ret = [];
 
-	console.log(key);
-	do {
-		await axios({
-			method : 'get',
-			url : uriBase + page,
-		}).then((res) => {
-			const list = res.data[key];
-			totalPages = res.data.totalPages;
-			ret = ret.concat(list);
-		});
-	} while (page++ < totalPages);
-	console.log(ret.length);
+  console.log(key);
+  do {
+    await axios({
+      method: "get",
+      url: uriBase + page,
+    }).then((res) => {
+      const list = res.data[key];
+      totalPages = res.data.totalPages;
+      ret = ret.concat(list);
+    });
+  } while (page++ < totalPages);
+  console.log(ret.length);
 
-	return ret;
+  return ret;
 }
 
 async function main() {
-	const stores  = req(storePage, 'storeInfos');
-	const sales   = req(salePage,  'sales');
-	let codeSales = {};
+  const stores = req(storePage, "storeInfos");
+  const sales = req(salePage, "sales");
+  let codeSales = {};
 
-	await sales.then((resolve) => {
-		resolve.forEach((sale)  => { 
-			if (sale.code) {
-				codeSales[sale.code] = sale;
-				delete(sale.code);
-			}
-		});
-	});
+  await sales.then((resolve) => {
+    resolve.forEach((sale) => {
+      if (sale.code) {
+        codeSales[sale.code] = sale;
+        delete sale.code;
+      }
+    });
+  });
 
-	await stores.then((resolve) => {
-		resolve.forEach((store) => {
-			if (codeSales[store.code]) {
-				storeInfos.push(Object.assign(store, codeSales[store.code]));
-			}
-		});
-	});
+  await stores.then((resolve) => {
+    resolve.forEach((store) => {
+      if (codeSales[store.code]) {
+        storeInfos.push(Object.assign(store, codeSales[store.code]));
+      }
+    });
+  });
 }
 
-app.get('/', (req, res) => {
-	if (req.query.city != null) {
-		console.log(req.query.city);
-		console.log(storeInfos.length);
+app.get("/", (req, res) => {
+  if (req.query.city != null) {
+    console.log(req.query.city);
+    console.log(storeInfos.length);
 
-		let city = req.query.city;
-		let storeList = [];
-		storeInfos.forEach((element) => {
-			if (element.addr.indexOf(city) >= 0) {
-				storeList.push(element);
-			}
-		});
-		res.send(storeList);
-	} else {
-		res.send('city=[city name]');
-	}
+    let city = req.query.city;
+    let storeList = [];
+    storeInfos.forEach((element) => {
+      if (element.addr.indexOf(city) >= 0) {
+        storeList.push(element);
+      }
+    });
+    res.send(storeList);
+  } else {
+    res.send("city=[city name]");
+  }
 });
 
-app.listen(port, () => { console.log(`server start port at ${port}`); });
+app.listen(port, () => {
+  console.log(`server start port at ${port}`);
+});
 
 main();
 console.log("start server");
